05caa4a55fd01556e686da63b6e12a55670226e3
VictorAguiare <victoraguiar018@gmail.comm>
2022-02-02 16:23:47 -0300

    dados da api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import react, {useEffect, useState} from 'react'
+import MovieRow from './Components/MovieRow'
+import tmdb from './tmdb'
 
-function App() {
-  return (
-    <div className="App">
-      
+export default ()=>{
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(()=>{
+    const loadAll = async ()=>{
+      //pegando a lista total
+      let list = await tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  },[])
+
+  return(
+    <div className='page'>  
+      <section className='lists'>
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
-  );
+  )
 }
-
-export default App;
